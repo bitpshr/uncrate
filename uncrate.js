e14ac13baadb0305b02c23b87200c08f526d34e1
Paul Bouchon <mail@bitpshr.net>
2018-11-30 22:00:02 -0500

    Update template

--- uncrate.js
+++ uncrate.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 const { dirname, join, relative } = require('path');
-const { emptyDirSync, ensureDirSync, readFileSync, writeFileSync } = require('fs-extra');
+const { ensureDirSync, readFileSync, removeSync, writeFileSync } = require('fs-extra');
 const markdown = require('markdown-it')().use(require('markdown-it-anchor'), {
 	permalink: true,
 	permalinkSymbol: '#'
 });
 
-const { description, exclude, logo, name, out, repo, projectVersion } = require('yargs')
+const args = require('yargs')
 	.option('d', { alias: 'description', desc: 'Generated site meta description' })
 	.option('e', { alias: 'exclude', desc: 'RegExp of paths to ignore', default: 'node_modules' })
 	.option('l', { alias: 'logo', desc: 'URL of a project logo image' })
@@ -15,36 +15,30 @@ const { description, exclude, logo, name, out, repo, projectVersion } = require(
 	.option('o', { alias: 'out', desc: 'Path where the site should be generated', default: 'dist' })
 	.option('p', { alias: 'projectVersion', desc: 'Project version' })
 	.option('r', { alias: 'repo', desc: 'URL of a project git repository' })
+	.option('s', { alias: 'subdir', desc: 'Path to use as a base for generated URLs', default: '/' })
 	.alias('h', 'help').argv;
 
+removeSync(args.out);
 const template = readFileSync(join(__dirname, 'template.html'), 'utf8');
 const tree = require('directory-tree')(process.cwd(), {
-	exclude: exclude && new RegExp(exclude),
+	exclude: new RegExp(args.exclude),
 	extensions: /\.md$/
 });
 
 const walk = children => {
-	children.sort().forEach(child => {
-		if (child.type === 'directory') {
-			child.children && walk(child.children);
-			return;
-		}
-		const html = require('ejs').render(template, {
-			content: markdown.render(readFileSync(child.path, 'utf8')),
-			cwd: process.cwd(),
-			description,
-			logo,
-			name,
-			projectVersion,
-			relative,
-			repo,
-			tree
+	children
+		.sort((a, b) => (a.name < b.name ? -1 : 1))
+		.forEach(child => {
+			if (child.type === 'directory') {
+				return child.children && walk(child.children);
+			}
+			const copy = markdown.render(readFileSync(child.path, 'utf8'));
+			const html = require('ejs').render(template, { args, copy, relative, tree });
+			const path = relative(process.cwd(), child.path.replace(/[\/\\]\d+_/g, '/'));
+			const file = join(process.cwd(), args.out, path).replace(/\.md$/, '.html');
+			ensureDirSync(dirname(file));
+			writeFileSync(file, html);
 		});
-		const newPath = join(process.cwd(), out, relative(process.cwd(), child.path)).replace(/\.md$/, '.html');
-		ensureDirSync(dirname(newPath));
-		writeFileSync(newPath, html);
-	});
 };
 
-emptyDirSync(out);
 walk(tree.children);
